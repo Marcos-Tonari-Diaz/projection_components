143dc2f44644bd161bedd0226c1e8a333a59a00e
Marcos Diaz <marcos.dure.diaz@gmail.com>
2022-05-24 16:38:04 -0300

    line example

---
+++ README.md
@@ -0,0 +1,11 @@
+# projection_components
+
+### ThreeJS components for a future Perspective Projection webpage
+
+This project uses only the ThreeJs noed module and webpack to facilitate prototyping.
+
+Steps to run:
+
+1. npm install
+2. npx webapck
+3. load dist/index.html through a http server (a simple http server like python3 -m http.server or VSCode's live server is enough)
--- src/cube.js
+++ src/cube.js
@@ -1,23 +1,36 @@
 import * as THREE from "three"
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial()
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+//create a blue LineBasicMaterial
+const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+const points = [];
+points.push(new THREE.Vector3(- 10, 0, 0));
+points.push(new THREE.Vector3(0, 10, 0));
+points.push(new THREE.Vector3(10, 0, 0));
 
-camera.position.z = 4;
-cube.rotation.x = 0.5;
-cube.rotation.y = 0.5;
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
 
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-}
-animate();
+const line = new THREE.Line(geometry, material);
+scene.add(line);
+// const geometry = new THREE.BoxGeometry();
+// const material = new THREE.MeshBasicMaterial()
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+
+// cube.rotation.x = 0.5;
+// cube.rotation.y = 0.5;
+
+renderer.render(scene, camera);
+// function animate() {
+//     requestAnimationFrame(animate);
+//     renderer.render(scene, camera);
+// }
+// animate();
