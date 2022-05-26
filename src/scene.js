c7996da4880d3461f9249a2e449892c19b19c88b
Marcos Diaz <marcos.dure.diaz@gmail.com>
2022-05-26 13:02:22 -0300

    staterd projection center basic geometry

--- src/scene.js
+++ src/scene.js
@@ -1,5 +1,5 @@
 import * as THREE from "three"
-
+import { ProjectionCenter } from "./projection_center";
 import { Vertex } from "./vertex";
 
 const scene = new THREE.Scene();
@@ -11,8 +11,19 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-let vertex = new Vertex(0, 0, 0);
+let vertices = []
+vertices.push(new Vertex(5, 0, 0, 'white'));
+vertices.push(new Vertex(5, 2, 0, 'white'));
+vertices.push(new Vertex(5, 0, 2, 'white'));
 
-scene.add(vertex.getMesh());
+let vertex_group = new THREE.Group();
+vertices.forEach((vertex) => vertex_group.add(vertex.getMesh()))
+
+let projection_center = new ProjectionCenter(0, 0, 0, 'crimson');
+let projected_lines_group = projection_center.projectLines(vertices)
+
+scene.add(projection_center.getMesh());
+scene.add(vertex_group);
+scene.add(projected_lines_group);
 
 renderer.render(scene, camera);
--- src/vertex.js
+++ src/vertex.js
@@ -1,13 +1,13 @@
 import * as THREE from "three"
 
 export class Vertex {
-    constructor(x, y, z) {
-        this.material = new THREE.MeshBasicMaterial()
+    constructor(x, y, z, color) {
+        this.material = new THREE.MeshBasicMaterial({ color: color })
         this.geometry = new THREE.SphereGeometry()
         this.mesh = new THREE.Mesh(this.geometry, this.material);
         this.mesh.position.set(x, y, z)
     }
     getMesh() {
-        return this.mesh
+        return this.mesh;
     }
 }
